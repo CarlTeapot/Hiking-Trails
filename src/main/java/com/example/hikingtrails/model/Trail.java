8912def5149a514ba92598cc17d6f783afbbe727
Mate <marev22@freeuni.edu.ge>
2024-02-12 00:06:55 +0400

    organized the directory

---
+++ src/main/java/com/example/hikingtrails/model/Trail.java
@@ -0,0 +1,5 @@
+package com.example.hikingtrails.model;
+
+
+public class Trail {
+}
---
+++ src/test/java/com/example/hikingtrails/HikingTrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hikingtrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingTrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
